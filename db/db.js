5795c00d52f98ded46c26696eb67da1ea1de3b99
Do Hyeong Kim <dhk21512@gmail.com>
2022-06-06 17:47:34 +0900

    firebase-admin

--- db/db.js
+++ db/db.js
@@ -1,4 +1,7 @@
 const initializeApp = require("firebase/app");
+const { initializeApp, applicationDefault, cert } = require("firebase-admin/app");
+const { getFirestore, Timestamp, FieldValue } = require("firebase-admin/firestore");
+const serviceAccount = require("../firebase-admin.json");
 const dotenv = require("dotenv");
 dotenv.config();
 
