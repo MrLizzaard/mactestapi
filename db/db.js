54ab84879384e4ea4298b666db8e9c97f98b53d3
Do Hyeong Kim <dhk21512@gmail.com>
2022-06-12 22:13:37 +0900

    firebase db test

--- db/db.js
+++ db/db.js
@@ -1,17 +1,40 @@
-const initializeApp = require("firebase/app");
+// const initializeApp = require("firebase/app");
 const { initializeApp, applicationDefault, cert } = require("firebase-admin/app");
 const { getFirestore, Timestamp, FieldValue } = require("firebase-admin/firestore");
 const serviceAccount = require("../firebase-admin.json");
 const dotenv = require("dotenv");
 dotenv.config();
 
-const firebaseConfig = {
-  apiKey: process.env.FIREBASE_APIKEY,
-  authDomain: process.env.FIREBASE_AUTHDOMAIN,
-  projectId: process.env.FIREBASE_PROJECT_ID,
-  storageBucket: process.env.FIREBASE_STORAGE_BUCKET,
-  messagingSenderId: process.env.FIREBASE_MESSAGING_SENDER_ID,
-  appId: process.env.FIREBASE_APP_ID,
-};
+initializeApp({
+  credential: cert(serviceAccount),
+});
 
-const app = initializeApp(firebaseConfig);
+const db = getFirestore();
+
+module.exports = db;
+
+// async function test() {
+//   try {
+//     const docRef = db.collection("users").doc("alovelace");
+
+//     await docRef.set({
+//       first: "Ada",
+//       last: "Lovelace",
+//       born: 1815,
+//     });
+//   } catch (err) {
+//     console.log(err);
+//   }
+// }
+
+// test();
+// const firebaseConfig = {
+//   apiKey: process.env.FIREBASE_APIKEY,
+//   authDomain: process.env.FIREBASE_AUTHDOMAIN,
+//   projectId: process.env.FIREBASE_PROJECT_ID,
+//   storageBucket: process.env.FIREBASE_STORAGE_BUCKET,
+//   messagingSenderId: process.env.FIREBASE_MESSAGING_SENDER_ID,
+//   appId: process.env.FIREBASE_APP_ID,
+// };
+
+// const app = initializeApp(firebaseConfig);
--- index.js
+++ index.js
@@ -2,13 +2,25 @@ const express = require("express");
 const cors = require("cors");
 const app = express();
 const port = process.env.PORT || 3006;
+const db = require("./db/db");
 
 app.use(cors());
 
-app.get("/", (req, res) => {
-  res.json({
-    success: true,
-  });
+app.get("/", async (req, res) => {
+  try {
+    const docRef = db.collection("users").doc("aturing");
+
+    const result = await docRef.set({
+      first: "Alan",
+      middle: "Mathison",
+      last: "Turing",
+      born: 1912,
+    });
+
+    res.send({ result });
+  } catch (err) {
+    console.log(err);
+  }
 });
 
 app.get("/test", (req, res) => {
